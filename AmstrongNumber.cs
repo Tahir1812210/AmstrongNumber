34b2cb154fd0c8222ff1942ca1a454caacece1b0
Tahirid1812210 <cs1812210@szabist.pk>
2024-05-01 22:36:26 +0300

    Add project files.

---
+++ AmstrongNumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmstrongNumber
+{
+    public class AmstrongNumber
+    {
+        public static int CountNumber(int number)
+        {
+            return number.ToString().Length;    
+        }
+
+        public static bool isAmstrong(int number)
+        {
+            int digits = CountNumber(number);
+
+            int originalNumber = number;
+
+            int sum = 0;
+
+            while (number > 0) 
+            {
+                int digit = number % 10;
+
+               ;
+
+                sum += (int)Math.Pow(digit, digits);
+
+                number /= 10;
+
+            }
+            return sum == originalNumber;
+        }
+
+
+        public static void GenerateAmstrong(int start , int end)
+        {
+            Console.WriteLine($"Amstrong Number between {start} and {end}");
+
+            for(int i = start; i < end; i++) 
+            {
+                if(isAmstrong(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AmstrongNumber; // Import the namespace where your class is defined
+
+namespace AmstrongNumber // Define your main application namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AmstrongNumber.GenerateAmstrong(1, 1000);
+
+            int testnumber = 153;
+            AmstrongNumber.isAmstrong(testnumber);
+
+            Console.WriteLine($"{testnumber} is an amstrong number");
+        }
+    }
+}
