e74ebfb4da2f0e4cbffb1bbba9d0876d1254f0a3
Tahir1812210 <tahirahmedsyed3@gmail.com>
2024-05-09 15:38:11 +0300

    cross word with no diagnols done

--- Program.cs
+++ Program.cs
@@ -29,10 +29,24 @@ namespace AmstrongNumber // Define your main application namespace
 
             //int value = FibonacciWithRecursion.Fibonacci(4);
             //Console.WriteLine(value);
-            int number = 134;
-            int value = GetTheOddPosition.GetOddPositionDigit(number);
-            Console.WriteLine(value);
+            //int number = 134;
+            //int value = GetTheOddPosition.GetOddPositionDigit(number);
+            //Console.WriteLine(value);
+            //Console.ReadLine();
+
+            char[,] crossword =
+            {
+            { 'e', 'l', 'e', 'p', 'h', 'a', 'n', 't' }, 
+            { 't', 'a', 'x', 'y', 'o', 'n', 'p', 'e' }, 
+            { 'l', 'e', 'a', 'o', 't', 'a', 'l', 'o' }, 
+            { 'p', 'h', 'l', 'l', 's', 'a', 'x', 'g' }, 
+            { 'a', 'y', 'x', 'b', 'l', 'o', 'b', 'o' }  
+            };
+
+
+            string animalName = "elephant";
+
+            int count = CrossWordNoDiaganols.CountWord(crossword, animalName);
+            Console.WriteLine(count);
             Console.ReadLine();
-        }
-    }
-}
+            } } }
